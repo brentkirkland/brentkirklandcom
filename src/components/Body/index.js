3d89a08b2a84111f422523ff5998758c7e5765ea
brentkirkland <brentckirkland@gmail.com>
2017-08-03 23:29:24 -0700

    new projects

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -12,22 +12,30 @@ class Header extends Component {
         </div>
         <div className='tab'>
           <h2>Education</h2>
-          <p><b>University of California, Santa Barbara</b></p>
+          <span><b>University of California, Santa Barbara</b></span>
           <p>B.E. in Computer Science, 2017</p>
         </div>
         <div className='tab'>
           <h2>Work</h2>
-          <p><b>Graphiq Inc.</b></p>
+          <span><b>Graphiq Inc.</b></span>
           <p>Mobile Product Intern, Summer 2016</p>
-          <p><b>ECI, UCSB</b></p>
+          <span><b>ECI, UCSB</b></span>
           <p>Student Network Technician, April 2014 - March 2015</p>
         </div>
         <div className='tab'>
           <h2>Projects</h2>
-          <p><b>FantasyPollster</b></p>
-          <p>Fantasy Sports meets the 2016 election. <a href='http://www.forbes.com/sites/johngreathouse/2016/03/11/trump-clinton-look-out-this-student-founded-startup-may-pick-the-next-president/#34f1d41e6171'>Forbes</a>.</p>
-          <p><b>Livv, Inc.</b></p>
-          <p>Open sourced live feed of whats going on around UCSB. <a href='https://github.com/brentkirkland/Livv'>iOS</a> / <a href='https://github.com/livvapp/Server'>Server</a>.</p>
+          <span><b>brentkirklandcom</b></span>
+          <p>This website's <a href='https://github.com/brentkirkland/brentkirklandcom'>source code</a>.</p>
+          <span><b>Soilwatch</b></span>
+          <p>Work in Progress (!!!). A soil analytics website that uses BLE devices to report soil data. <a href='https://github.com/brentkirkland/slurp'>Front End</a>, <a href='https://github.com/brentkirkland/slurp-backend'>Back End</a>, <a href='https://github.com/brentkirkland/miflora'>Device</a>.</p>
+          <span><b>Pixxiti</b></span>
+          <p>A Reddit Place clone with extra features. <a href='https://github.com/brentkirkland/pixxiti'>Source Code</a>.</p>
+          <span><b>GoBang Bot</b></span>
+          <p>A GoBang Bot implemented using the Alpha-Beta Pruning Algorithm. <a href='https://github.com/brentkirkland/gobang-bot'>Source Code</a>.</p>
+          <span><b>Paxos and Map Reduce</b></span>
+          <p>Paxos concensus algorithm with Map Reduce. <a href='https://github.com/brentkirkland/paxos-map-reduce'>Source Code</a>.</p>
+          <span><b>Naive Bayes Classifier</b></span>
+          <p>A Naive Bayes implementation to find if a movie review was positive or negative. <a href='https://github.com/brentkirkland/naive-bayes-classifier'>Source Code</a>.</p>
           <br />
           <p>More projects on my <a href='https://github.com/brentkirkland'>Github</a>.</p>
         </div>
