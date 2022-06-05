acda876dcc7027f396f4c7aec00993a8bfe8e9ad
Brent Kirkland <brentckirkland@gmail.com>
2022-06-05 12:45:51 -0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,21 +4,41 @@ const IndexPage = () => (
   <Layout title="Brent Kirkland">
     <>
       <h1 className="text-2xl">Brent Kirkland</h1>
-      <ul>
-        <li>
-          Software Engineer at{" "}
-          <a className="text-indigo-600" href="https://weedmaps.com">
-            Weedmaps
+      <div className="py-4">
+        <h2 className="font-bold">Projects</h2>
+        <p>
+          <a className="text-indigo-600" href="https://goerli.pixxiti.com">
+            Pixxiti
           </a>
-        </li>
-        <li>
-          Previous Engineer at{" "}
-          <a className="text-indigo-600" href="https://bitfinex.com">
-            Bitfinex
-          </a>
-        </li>
-        <li>Computer Science BS from UCSB</li>
-        <li>
+          , Your NFT here
+        </p>
+      </div>
+      <div>
+        <h2 className="font-bold">Work Experience</h2>
+        <ul>
+          <li>
+            Software Engineer at{" "}
+            <a className="text-indigo-600" href="https://weedmaps.com">
+              Weedmaps
+            </a>
+            , <i>2019 - Present</i>
+          </li>
+          <li>
+            Software Engineer at{" "}
+            <a className="text-indigo-600" href="https://bitfinex.com">
+              Bitfinex
+            </a>
+            , <i>2017 - 2019</i>
+          </li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="py-4">
+        <h2 className="font-bold">Education</h2>
+        <p>Computer Science BS from UCSB</p>
+      </div>
+      <div>
+        <p>
           Contact me on{" "}
           <a
             className="text-indigo-600"
@@ -26,8 +46,17 @@ const IndexPage = () => (
           >
             LinkedIn
           </a>
-        </li>
-      </ul>
+        </p>
+        <p>
+          View website source code on{" "}
+          <a
+            className="text-indigo-600"
+            href="https://www.linkedin.com/in/brentland/"
+          >
+            Github
+          </a>
+        </p>
+      </div>
     </>
   </Layout>
 );
